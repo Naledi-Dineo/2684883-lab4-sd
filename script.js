ac4d1e812fb31c01b0a17f4e5559c3e28cd279da
Naledi-Dineo <naledimogomotsi3@gmail.com>
2025-03-10 16:43:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,35 +8,32 @@
     if(!countryName){
       alert('Country not entered');
     }
-    fetchCountry(countryName);
-  });
-
-  const fecthCountry = async (country) =>{
+  
     try {
-    const response = await fetch('https://restcountries.com/v3.1/name/{country}?fullText=true');
+    const response = await fetch('https://restcountries.com/v3.1/name/{countryName}?fullText=true');
     if (!response.ok) {
       throw new Error("Input full country name");
     }
 
     const data = await response.json();
-    countryinfo(data);
       
     } catch (error) {
       console.error(error.message);
     }  
-  }
+  
 
-  const countryinfo = async (data) =>{
     const countryInfo = document.getElementById('country-info');
     const bodering = document.getElementById('bordering-countries');
 
     countryInfo.innerHTML = 
-      <p>${data.capital[0]}</p>
-      <p>${data.population.toString()}</p>
-      <p>${data.region}</p>
-      <img src = "${data.image.png}" width "150">
+      <p>${data[0].capital[0]}</p>
+      <p>${data[0].population.toString()}</p>
+      <p>${data[0].region}</p>
+      <img src = "${data[0].image.png}" width "150">
       ;
-  }
+  });
+
+  
   
 
 

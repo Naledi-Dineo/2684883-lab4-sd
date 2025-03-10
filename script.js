064b7380c30677c758a4acf11391730d0ba9937c
Naledi-Dineo <naledimogomotsi3@gmail.com>
2025-03-10 15:55:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const myButton = document.getElementById('button');
+
+  myButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    const countryName = document.getElementById('name').value.trim();
+    
+    if(!countryName){
+      alert('Country not entered');
+    }
+    fetchCountry(countryName)
+  });
+
+  const fecthCountry = async (country) =>{
+    try {
+    const response = await fetch('https://restcountries.com/v3.1/name/{country}?fullText=true');
+    if (!response.ok) {
+      throw new Error("Input full country name");
+    }
+
+    const data = await response.json();
+    countryinfo(data);
+      
+    } catch (error) {
+      console.error(error.message);
+    }  
+  }
+
+  const countryinfo = async (data) =>{
+    const countryInfo = document.getElementById('country-info');
+    const bodering = document.getElementById('bordering-countries');
+
+    countryInfo.innerHTML
+    
+  }
+  
+});
+
+
+
 
